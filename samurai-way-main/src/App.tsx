5ed695040c60a60a3a5dab7ab487cb92a69ad8af
luciphoree <luciphoree@gmail.com>
2022-10-11 12:39:47 +0300

    dz5

--- samurai-way-main/src/App.tsx
+++ samurai-way-main/src/App.tsx
@@ -2,11 +2,38 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="App">
+            Hello, samurai! Let's go!
+        </div>
+    );
 }
 
+
+function Rating() {
+    return (
+        <>
+            <div>start</div>
+            <div>start</div>
+            <div>start</div>
+            <div>start</div>
+            <div>start</div>
+        </>
+    )
+}
+
+function Accordion(){
+    return (
+        <>
+            <h3>Меню</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+            </ul>
+        </>
+    )
+}
 export default App;
